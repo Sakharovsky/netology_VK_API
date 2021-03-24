521fce5e1f8b6fff1563ff832e83fb282b9e88ad
Sergey Sakharovsky <sergey.sakharovskiy@gmail.com>
2021-03-24 10:57:20 +0300

    Deleted classes, will stay with 2 functions (download_url & upload_url). Now the first function is almost released. Yet need to deal with the umixtime.

--- VK_API.py
+++ VK_API.py
@@ -1,31 +1,38 @@
 import requests
 import pprint
 
-class VkUser:
-    url = 'https://api.vk.com/method/'
-    version = '5.130'
 
-    def __init__(self, token):
-        self.token = token
-        self.params = {
-            'access_token': self.token,
-            'v': self.version     
-        }
-        self.owner_id = requests.get(self.url+'users.get', self.params).json()['response'][0]['id']
+def get_vk_pics(user_id=None, album_id='profile', numb_photos=5):
+    url_vk = 'https://api.vk.com/method/'
+    params = {
+        'access_token': '',
+        'v': "5.130"
+    }
+    owner_id = requests.get(url_vk + 'users.get', params).json()['response'][0]['id']
+
+    if user_id == None:
+        user_id = owner_id
     
+    params_photos = {
+        'owner_id': user_id,
+        'album_id': album_id,
+        'count': numb_photos,
+        'rev': 1,
+        'extended': 1
+    }
+
+    res = requests.get(url_vk + "photos.get", params={**params, **params_photos})
+
+    # создаём список последних фотографий
+    # определяем, если в списке уже есть фотография с таким же кол-вом лайков
+    pic_list = {}
+    for picture in res.json()['response']['items']:
+        if str(picture['likes']['count']) in pic_list.keys(): 
+            pic_list.update({'{} ({})'.format(picture['likes']['count'], picture['date']): picture['sizes'][len(picture['sizes'])-1]['url']})
+        else:
+            pic_list.update({'{}'.format(picture['likes']['count']): picture['sizes'][len(picture['sizes'])-1]['url']})
     
-    def get_followers(self, user_id=None):
-        if user_id is None:
-            user_id = self.owner_id
-        followers_url = self.url + 'users.getFollowers'
-        followers_params = {
-            'count': 1000,
-            'user_id': user_id
-        }
-        res = requests.get(followers_url, params={**self.params, **followers_params})
-        return res.json()
+    return pic_list
 
-admin = VkUser('')
-
-pprint.pprint(admin.get_followers())
+pprint.pprint(get_vk_pics())
 
